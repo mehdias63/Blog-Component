8384e9270e479d38c50f897919b868ef5205ac95
Mehdi Asadi <mehdiasadi63@gmail.com>
2025-05-25 19:57:10 +0330

    add commentForm

--- src/app/(blogs)/blogs/_components/comment/PostComment.jsx
+++ src/app/(blogs)/blogs/_components/comment/PostComment.jsx
@@ -5,25 +5,46 @@ import classNames from 'classnames'
 import Comment from './Comment'
 import Modal from '@/ui/Modal'
 import { useState } from 'react'
+import CommentForm from './CommentForm'
+import { useAuth } from '@/context/AuthContext'
+import { useRouter } from 'next/navigation'
 
 function PostComment({ post: { comments, _id: postId } }) {
-	const [open, setOpen] = useState(true)
+	const [open, setOpen] = useState(false)
+	const [parent, setParent] = useState(null)
+	const { user } = useAuth()
+	const router = useRouter()
+
+	const addNewCommentHandler = parent => {
+		if (!user) {
+			router.push('/signin')
+			return
+		}
+		setParent(parent)
+		setOpen(true)
+	}
 
 	return (
 		<div className="mb-10">
 			<Modal
 				open={open}
 				onClose={() => setOpen(false)}
-				title="mehdi"
-				description="asadi"
+				title={parent ? 'پاسخ به نظر' : 'نظر جدید'}
+				description={
+					parent ? parent.user.name : 'نظر خود را وارد کنید'
+				}
 			>
-				content testy....
+				<CommentForm />
 			</Modal>
 			<div className="flex flex-col items-center lg:flex-row justify-between gap-y-3 mb-8">
 				<h2 className="text-2xl font-bold text-secondary-800">
 					نظرات
 				</h2>
-				<Button variant="outline" className="flex items-center py-2">
+				<Button
+					variant="outline"
+					className="flex items-center py-2"
+					onClick={() => addNewCommentHandler(null)}
+				>
 					<QuestionMarkCircleIcon className="w-4 ml-2" />
 					<span>ثبت نظر جدید</span>
 				</Button>
